37c0888947ea080aab59e46a57973a35fa3df5a1
vmescher <mescher174@gmail.com>
2022-12-01 00:58:29 +0500

    fix(lint): Исправил все ошибки в стилях и скриптах, настроил линтеры

--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,33 +1,31 @@
-export class EventBus<E extends Record<string, unknown[]>> {
-    private readonly listeners: {
-        [K in keyof E]?: Array<(...args: E[K]) => void>;
-    } = {};
+export default class EventBus<E extends Record<string, unknown[]>> {
+	private readonly listeners: {
+		[K in keyof E]?: Array<(..._args: E[K]) => void>;
+	} = {};
 
-    on<K extends keyof E>(event: K, callback: (...args: E[K]) => void) {
-        if (!this.listeners[event]) {
-            this.listeners[event] = [];
-        }
+	on<K extends keyof E>(event: K, callback: (..._args: E[K]) => void) {
+		if (!this.listeners[event]) {
+			this.listeners[event] = [];
+		}
 
-        this.listeners[event]!.push(callback);
-    }
+		this.listeners[event]!.push(callback);
+	}
 
-    off<K extends keyof E>(event: K, callback: () => void) {
-        if (!this.listeners[event]) {
-            throw new Error(`Нет события: ${event as string}`);
-        }
+	off<K extends keyof E>(event: K, callback: () => void) {
+		if (!this.listeners[event]) {
+			throw new Error(`Нет события: ${event as string}`);
+		}
 
-        this.listeners[event] = this.listeners[event]!.filter(
-            listener => listener !== callback
-        );
-    }
+		this.listeners[event] = this.listeners[event]!.filter((listener) => listener !== callback);
+	}
 
-    emit<K extends keyof E>(event: K, ...args: E[K]) {
-        if (!this.listeners[event]) {
-            throw new Event(`Нет события: ${event as string}`);
-        }
+	emit<K extends keyof E>(event: K, ...args: E[K]) {
+		if (!this.listeners[event]) {
+			throw new Event(`Нет события: ${event as string}`);
+		}
 
-        this.listeners[event]!.forEach(listener => {
-            listener(...args);
-        });
-    }
+		this.listeners[event]!.forEach((listener) => {
+			listener(...args);
+		});
+	}
 }
