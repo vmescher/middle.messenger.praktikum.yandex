e529147b755ec7f597b402e75ff8feeec0eabcf4
vmescher <mescher174@gmail.com>
2022-11-30 22:32:32 +0500

    feat(lint): Добавил stylelint и eslint

--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,9 +1,9 @@
 export class EventBus<E extends Record<string, unknown[]>> {
     private readonly listeners: {
-        [K in keyof E]?: Array<(...args: unknown[]) => void>;
+        [K in keyof E]?: Array<(...args: E[K]) => void>;
     } = {};
 
-    on<K extends keyof E>(event: K, callback: (...args: unknown[]) => void) {
+    on<K extends keyof E>(event: K, callback: (...args: E[K]) => void) {
         if (!this.listeners[event]) {
             this.listeners[event] = [];
         }
@@ -11,7 +11,7 @@ export class EventBus<E extends Record<string, unknown[]>> {
         this.listeners[event]!.push(callback);
     }
 
-    off<K extends keyof E>(event: K, callback: (...args: unknown[]) => void) {
+    off<K extends keyof E>(event: K, callback: () => void) {
         if (!this.listeners[event]) {
             throw new Error(`Нет события: ${event as string}`);
         }
@@ -23,8 +23,7 @@ export class EventBus<E extends Record<string, unknown[]>> {
 
     emit<K extends keyof E>(event: K, ...args: E[K]) {
         if (!this.listeners[event]) {
-            return;
-            // throw new Event(`Нет события: ${event}`);
+            throw new Event(`Нет события: ${event as string}`);
         }
 
         this.listeners[event]!.forEach(listener => {
